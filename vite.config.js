64c8485253a4278c3d5022b2328097f51d4d5c7f
cteheranr <kmilo.teheran@gmail.com>
2025-09-21 15:10:28 -0500

    cambios

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://https://cteheranr.github.io/invitacion-jr'
 })
